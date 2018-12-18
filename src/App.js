3c4aa2c73db9172bfeeba88d5f8f663755258806
Charles Loehle <clguitar1@gmail.com>
2018-12-18 13:53:50 -0500

    input controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
+import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=' + this.state.paras + '&html=' + this.state.html)
+      .then((response) => {
+        this.setState({ text: response.data }, function () {
+          // console.log(this.state);
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  showHtml(x) {
+    // update html property in state, then call getSampleText to update the request
+    this.setState({ html: x }, this.getSampleText);
+  }
+
+  changeParas(number) {
+    // update paras property in state, then call getSampleText to update the request
+    this.setState({ paras: number }, this.getSampleText);
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>TEST 123</h2>
+        <h1>Random Pain Relief Exercise Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <Output value={this.state.text} />
       </div>
     );
   }
